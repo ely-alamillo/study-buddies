18b1b28d5771b2328c5881eb47ed51b05592e1b7
Ely Alamillo <ely0013@gmail.com>
2017-09-22 16:25:52 -0500

    added route for Feed container

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import Register from './pages/Register';
 import Feed from './pages/Feed';
 import Login from './pages/Login';
+import SingleGroup from './pages/SingleGroup';
+import Navigation from './pages/Navigation'
 
 import { Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/* <div className="App-header">
           <h2>studyBuddies</h2>
-        </div>
+        </div> */}
+        <Navigation />
         <Route path='/' exact component={Register} />
-        <Route path='/feed' component={Feed} />
+        <Route path='/feed' exact component={Feed} />
         <Route path='/login' component={Login} />
+        <Route path='/feed/:id'  component={SingleGroup} />
       </div>
     );
   }
