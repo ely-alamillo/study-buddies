f87de9361afdd0e2b1621dff9c13d76b7b3eb82e
Ely Alamillo <ely0013@gmail.com>
2017-09-24 18:54:14 -0500

    added style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { login } from '../actions';
 
-import { FormControl, FormGroup } from 'react-bootstrap';
+import { FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 
 
 class Login extends Component {
@@ -35,28 +35,32 @@ class Login extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <form className='login-form' onSubmit={this.handleLogin}>
-        <FormGroup>
-          username
-          <FormControl
-            type='text'
-            placeholder='username'
-            value={this.state.username}
-            onChange={this.handleUsername}
-          />
-        </FormGroup>
-        <FormGroup>
-          password
-          <FormControl
-            type='text'
-            placeholder='password'
-            value={this.state.password}
-            onChange={this.handlePassword}
-          />
-          <button className='btn btn-success' type='submit'> Login </button>
-        </FormGroup>
-      </form>
+      <div className='container auth-form'>
+        <div className='jumbotron login-container'>
+          <form onSubmit={this.handleRegister}>
+            <FormGroup>
+              <ControlLabel>Username</ControlLabel>
+              <FormControl
+                type='text'
+                placeholder='username'
+                value={this.state.username}
+                onChange={this.handleUsername}
+                className='input-format'
+              />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                type='text'
+                placeholder='password'
+                value={this.state.password}
+                onChange={this.handlePassword}
+                className='input-format'
+              />
+              <button className='btn btn-success' type='submit'> Register </button>
+            </FormGroup>
+          </form>
+        </div>
       </div>
     )
   }
