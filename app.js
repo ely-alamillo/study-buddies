6fea811479995ca29386c2cf48244417179a0d41
Ely Alamillo <ely0013@gmail.com>
2017-09-24 19:12:13 -0500

    added mlab uri

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/study-buddies', {useMongoClient: true});
+// use for local db
+// mongoose.connect('mongodb://localhost/study-buddies', {useMongoClient: true});
+
+// use for heroku db
+mongoose.connect('mongodb://heroku_5v62q657:doo5r3odvn05hh8qr0gggd7rhh@ds149324.mlab.com:49324/heroku_5v62q657', {useMongoClient: true});
 
 const corsOptions = {
   "origin": "http://localhost:3000",
