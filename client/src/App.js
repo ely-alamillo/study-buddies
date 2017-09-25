c26cfbe30766bd51ecbe86def8bd90b61ce6da7d
Ely Alamillo <ely0013@gmail.com>
2017-09-24 22:42:24 -0500

    added styles

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="">
-        {/* <div className="App-header">
-          <h2>studyBuddies</h2>
-        </div> */}
+      <div className="App">
         <Navigation />
         <Route path='/' exact component={Register} />
         <Route path='/feed' exact component={Feed} />
